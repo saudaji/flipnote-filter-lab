1756a73a0e053e11abf15f23746f5924b028d0c2
Sagredo <tu-email@gmail.com>
2026-07-22 15:09:53 -0600

    [fix] lab: bump CACHE v82 (T9 mergeado)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flipnote-filter-lab-v81-pipeline-tiempo';
+const CACHE = 'flipnote-filter-lab-v82-perf-interim';
 const CDN_CACHE = 'flip-cdn-v1';
 const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './pretext.js'];
 const FONT_ASSETS = [
