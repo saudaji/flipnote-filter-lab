b042859b5afacfdb6cee7d239233f10f947a17f4
Sagredo <tu-email@gmail.com>
2026-07-23 13:14:39 -0600

    [fix] lab: bump CACHE v96 + FLIP_BUILD v96 (T26 mergeado)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flipnote-filter-lab-v95-termico-caps';
+const CACHE = 'flipnote-filter-lab-v96-termico-gpu';
 const CDN_CACHE = 'flip-cdn-v1';
 const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './pretext.js'];
 const FONT_ASSETS = [
